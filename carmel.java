de42adc98161dd7cfcab28eea3c0069d1be7c7ce
carmelbar24 <95103@ganyavne.ort.org.il>
2023-03-13 10:36:10 +0200

    carmel update

--- carmel.java
+++ carmel.java
@@ -1,7 +1,7 @@
-public class Hello {
+public class Carmel {
 	public static void main(String [] args){
 		for (int i=1;i<=10;i++){
-			System.out.println("carmel bar..");
+			System.out.println("carmel bar.."+i);
 			}
 		}
 	}
